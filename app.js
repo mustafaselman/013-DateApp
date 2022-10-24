5b7f9961517baf5ad7cdbea410ebea42610e7365
mustafaselmanonal <mustafaselman.100@gmail.com>
2022-10-24 21:55:15 +0300

    fix

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ function currentTimeRotation(sec, min, hr)
     hr_rotation = 30 * hr + min / 2; //converting current time
     min_rotation = 6 * min;
     sec_rotation = 6 * sec;
-    console.log(hr + ":" + min + ":" + sec);
     hour.style.transform = `rotate(${hr_rotation}deg)`;
     minute.style.transform = `rotate(${min_rotation}deg)`;
     second.style.transform = `rotate(${sec_rotation}deg)`;
@@ -25,23 +24,26 @@ function currentTimeDigital(sec, min, hr)
     if (hr<10){
         digitalClock.innerHTML = '0'+ hr + ':' + min + ':' + sec;
     }
-    if (min<10){
+    else if (min<10){
         digitalClock.innerHTML =  hr + ':' + '0'+ min + ':' + sec;
     }
-    if (sec<10){
+    else if (sec<10){
         digitalClock.innerHTML = hr + ':' + min + ':' + '0'+ sec;
     }
-    if (min<10 && sec<10){
+    else if (min<10 && sec<10){
         digitalClock.innerHTML =  hr + ':' + '0'+ min + ':' + '0'+ sec;
     }
-    if (hr<10 && sec<10){
+    else if (hr<10 && sec<10){
         digitalClock.innerHTML = '0'+ hr + ':' +  min + ':' + '0'+ sec;
     }
-    if (min<10 && hr<10){
+    else if (min<10 && hr<10){
         digitalClock.innerHTML = '0'+ hr + ':' + '0'+  min + ':' + sec;
     }
-    if (min<10 && hr<10 && sec<10){
+    else if (min<10 && hr<10 && sec<10){
         digitalClock.innerHTML = '0'+ hr + ':' + '0'+  min + ':' +'0'+ sec;
+    }
+    else {
+        digitalClock.innerHTML =  hr + ':' +  min + ':' + sec; 
     }
 
 }
